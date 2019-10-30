e4dde9700dd23f80b60904ef98cf9bac3294646c
sseen <ssnfee@gmail.com>
2019-10-30 10:41:19 +0800

    add swift basic content

---
+++ MySwiftPlayground.playground/Contents.swift
@@ -0,0 +1,98 @@
+import UIKit
+
+var str = "Hello, playground"
+
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+
+greet(person: "Bob", day: "Tuesday")
+
+// 形式参数 代替 实际参数
+func greet(_ person:String, on day:String) -> String {
+    return "Hello \(person), today is \(day)"
+}
+
+greet("John", on: "wednesday")
+
+
+// 元组 返回值
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.2)
+
+// 多个参数
+func sumOf(numbers: Int...) -> Int {
+    var sum = 0
+    for number in numbers {
+        sum += number
+    }
+    return sum
+}
+sumOf()
+sumOf(numbers: 42, 597, 12)
+
+// 函数 内嵌
+func returnFifteen() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+returnFifteen()
+
+// 函数 作为返回值
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+increment(7)
+
+// 函数 作为参数
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 7, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
+
+// 函数其实就是必包的一种特殊形式，一段可以随后调用的代码块
+numbers.map { (number:Int) -> Int in
+    let result = 3 * number
+    return result
+}
+
+//: 闭包类型可知，参数类型，返回类型，那么这些都可以去掉
+let mappedNumbers = numbers.map { number in 3 * number }
+print(mappedNumbers)
+
+let sortedNumbers = numbers.sorted { $0 > $1 }
+print(sortedNumbers)
