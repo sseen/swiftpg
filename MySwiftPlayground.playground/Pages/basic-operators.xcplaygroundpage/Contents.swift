035616441a51001b2abf878a61eb6a8730f810f6
sseen <ssnfee@gmail.com>
2019-11-01 15:01:37 +0800

    basic operators done

--- MySwiftPlayground.playground/Pages/basic-operators.xcplaygroundpage/Contents.swift
+++ MySwiftPlayground.playground/Pages/basic-operators.xcplaygroundpage/Contents.swift
@@ -1,7 +1,74 @@
 //: [Previous](@previous)
+//: # 基本运算符
 
 import Foundation
 
-var str = "Hello, playground"
+//: 余数  a = (b x some multiplier) + remainder
+let b = 9 % 4
+
+let name = "world"
+if name == "world" {
+    print("hello, world")
+} else {
+    print("I'm sorry \(name), but I don't recognize you")
+}
+
+// 三元运算 ?:
+let contentHeight = 40
+let hasHeader = true
+let rowHeight = contentHeight + (hasHeader ? 50 : 20)
+
+// 合并空值运算 ??
+let defaultColorName = "red"
+var userDefinedColorName: String? // defaults to nil
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+
+// 区间，闭区间运算
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+
+// 区间，半开区间
+let names = ["Anna", "Alex", "Brian", "Jack"]
+let count = names.count
+for i in 0..<count {
+    print("Person \(i + 1) is called \(names[i])")
+}
+
+// 区间，单侧区间
+for name in names[2...] {
+    print(name)
+}
+ 
+for name in names[...2] {
+    print(name)
+}
+
+let range = ...5
+range.contains(7)   // false
+range.contains(4)   // true
+range.contains(-1)  // true
+
+//: 逻辑运算符
+let allowedEntry = false
+if !allowedEntry {
+    print("ACCESS DENIED")
+}
+
+let enteredDoorCode = true
+let passedRetinaScan = false
+if enteredDoorCode && passedRetinaScan {
+    print("Welcome!")
+} else {
+    print("ACCESS DENIED")
+}
+
+let hasDoorKey = false
+let knowsOverridePassword = true
+if hasDoorKey || knowsOverridePassword {
+    print("Welcome!")
+} else {
+    print("ACCESS DENIED")
+}
 
 //: [Next](@next)
