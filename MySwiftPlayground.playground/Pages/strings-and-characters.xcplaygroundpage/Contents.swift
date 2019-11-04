43f0d80e291233631e9864e91e25701e5ffd7a1b
sseen <ssnfee@gmail.com>
2019-11-04 13:15:01 +0800

    字符串 done

--- MySwiftPlayground.playground/Pages/strings-and-characters.xcplaygroundpage/Contents.swift
+++ MySwiftPlayground.playground/Pages/strings-and-characters.xcplaygroundpage/Contents.swift
@@ -62,12 +62,12 @@ print(catString)
 // 连接
 let string1 = "hello"
 let string2 = " there"
-var welcome = string1 + string2
+var welcome1 = string1 + string2
 
 var instruction = "look over"
 instruction += string2
 
-welcome.append(exclamationMark)
+welcome1.append(exclamationMark)
 
 let badStart = """
 one
@@ -93,7 +93,125 @@ print(#"Write an interpolated string in Swift using \(multiplier)."#)
 print(#"6 times 7 is \#(6 * 7)."#)
 print("6 times 7 is \(6 * 7).")
 
+// 扩展字形集群
+let eAcute: Character = "\u{E9}" // é
+let combinedEAcute: Character = "\u{65}\u{301}" // e followed by
+let precomposed: Character = "\u{D55C}" // 한
+let decomposed: Character = "\u{1112}\u{1161}\u{11AB}" // ᄒ, ᅡ, ᆫ
+let enclosedEAcute: Character = "\u{E9}\u{20DD}"
+let regionalIndicatorForUS: Character = "\u{1F1FA}\u{1F1F8}"
+
+// 字符统计
+let unusualMenagerie = "Koala 🐨, Snail 🐌, Penguin 🐧, Dromedary 🐪"
+print("unusualMenagerie has \(unusualMenagerie.count) characters")
+    
+var word = "cafe"
+print("the number of characters in \(word) is \(word.count)")
+ 
+word += "\u{301}"    // COMBINING ACUTE ACCENT, U+0301
+ 
+print("the number of characters in \(word) is \(word.count) \(regionalIndicatorForUS)")
+
+// 索引
+// 不同的字符会获得不同的内存空间来储存，所以为了明确哪个 Character 在哪个特定的位置，你必须从 String的开头或结尾遍历每一个 Unicode 标量。因此，Swift 的字符串不能通过整数值索引。
+let greeting = "Guten Tag!"
+greeting[greeting.startIndex]
+greeting[greeting.index(before: greeting.endIndex)]
+greeting[greeting.index(after: greeting.startIndex)]
+let index = greeting.index(greeting.startIndex, offsetBy: 7)
+greeting[index]
+
+for index in greeting.indices {
+    print("\(greeting[index]) ", terminator: "")
+}
+
+// 插入 删除
+var welcome = "hello"
+welcome.insert("!", at: welcome.endIndex)
+welcome.insert(contentsOf: " there", at: welcome.index(before: welcome.endIndex))
+
+welcome.remove(at: welcome.index(before: welcome.endIndex))
+let range = welcome.index(welcome.endIndex, offsetBy: -6)..<welcome.endIndex
+welcome.removeSubrange(range)
+// substring，性能的优化，可以使用原有string的存储空间（避免内存拷贝），显示成为string时候，才新开辟内存空间
+let greeting2 = "Hello, world!"
+let index2 = greeting.firstIndex(of: ",") ?? greeting2.endIndex
+let beginning = greeting2[..<index]
+let newString = String(beginning)
+
+// 比较
+let quotation2 = "We're a lot alike, you and I."
+let sameQuotation = "We're a lot alike, you and I."
+if quotation2 == sameQuotation {
+    print("These two strings are considered equal")
+}
+
+let eAcuteQuestion = "Voulez-vous un caf\u{E9}?"
+let combinedEAcuteQuestion = "Voulez-vous un caf\u{65}\u{301}?"
+if eAcuteQuestion == combinedEAcuteQuestion {
+    print("These two strings are considered equal")
+}
+
+let latinCapitalLetterA: Character = "\u{41}"
+let cyrillicCapitalLetterA: Character = "\u{0410}"
+if latinCapitalLetterA != cyrillicCapitalLetterA {
+    print("These two characters are not equivalent")
+}
+
+// 比较 前缀后缀相等性
+let romeoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
+
+var act1SceneCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasPrefix("Act 1 ") {
+        act1SceneCount += 1
+    }
+}
+print("There are \(act1SceneCount) scenes in Act 1")
+
+var mansionCount = 0
+var cellCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasSuffix("Capulet's mansion") {
+        mansionCount += 1
+    } else if scene.hasSuffix("Friar Lawrence's cell") {
+        cellCount += 1
+    }
+}
+print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
+
+// 编码
+let dogString = "Dog‼🐶"
+for codeUnit in dogString.utf8 {
+    print("\(codeUnit) ", terminator: "")
+}
+print("")
+for codeUnit in dogString.utf16 {
+    print("\(codeUnit) ", terminator: "")
+}
+print("")
+for scalar in dogString.unicodeScalars {
+    print("\(scalar.value) ", terminator: "")
+}
+print("")
+for scalar in dogString.unicodeScalars {
+    print("\(scalar) ")
+}
+
+
 
 //: ### 总结
-//: 扩展字符串分隔符
+//: 扩展字符串分隔符 扩展字形集群
 //: [Next](@next)
