b83c835d684b8fc52bea9ea5b5f43386cb0f90ef
xu Sun <ssnfee@gmail.com>
2019-11-23 15:44:59 +0800

    方法done

--- MySwiftPlayground.playground/Pages/methods.xcplaygroundpage/Contents.swift
+++ MySwiftPlayground.playground/Pages/methods.xcplaygroundpage/Contents.swift
@@ -3,7 +3,138 @@
 
 import Foundation
 
-var str = "Hello, playground"
+//: 实例方法
+
+class Counter {
+    var count = 0
+    func increment() {
+        count += 1
+    }
+    func increment(by amount: Int) {
+        count += amount
+    }
+    func reset() {
+        count = 0
+    }
+}
+
+let counter = Counter()
+counter.increment()
+counter.increment(by: 5)
+counter.reset()
+
+struct Point1 {
+    var x = 0.0, y = 0.0
+    func isToTheRightOf(x: Double) -> Bool {
+        return self.x > x
+    }
+}
+let somePoint1 = Point1(x: 4.0, y: 5.0)
+if somePoint1.isToTheRightOf(x: 1.0) {
+    print("This point is to the right of the line where x == 1.0")
+}
+
+// 变异 mutating
+struct Point {
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        x += deltaX
+        y += deltaY
+    }
+}
+var somePoint = Point(x: 1.0, y: 1.0)
+somePoint.moveBy(x: 2.0, y: 3.0)
+print("The point is now at (\(somePoint.x), \(somePoint.y))")
+
+// 你不能在常量结构体类型里调用异变方法，因为自身属性不能被改变，就算它们是变量属性：
+//let fixedPoint = Point(x: 3.0, y: 3.0)
+//fixedPoint.moveBy(x: 2.0, y: 3.0)
+// this will report an error
+
+// error: left side of mutating operator isn't mutable: 'self' is immutabley += deltaY
+
+//struct Point2 {
+//    var x = 0.0, y = 0.0
+//    func moveBy(x deltaX: Double, y deltaY: Double) {
+//        x += deltaX
+//        y += deltaY
+//    }
+//}
+//var somePoint2 = Point2(x: 1.0, y: 1.0)
+//somePoint2.moveBy(x: 2.0, y: 3.0)
+//print("The point is now at (\(somePoint2.x), \(somePoint2.y))")
+
+// 隐藏 self
+struct Point3 {
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        self = Point3(x: x + deltaX, y: y + deltaY)
+    }
+}
+
+enum TriStateSwitch {
+    case off, low, high
+    mutating func next() {
+        switch self {
+        case .off:
+            self = .low
+        case .low:
+            self = .high
+        case .high:
+            self = .off
+        }
+    }
+}
+var ovenLight = TriStateSwitch.low
+ovenLight.next()
+ovenLight.next()
+
+//: 类型方法
+struct LevelTracker {
+    static var highestUnlockedLevel = 1
+    var currentLevel = 1
+    
+    static func unlock(_ level: Int) {
+        if level > highestUnlockedLevel { highestUnlockedLevel = level }
+    }
+    
+    static func isUnlocked(_ level: Int) -> Bool {
+        return level <= highestUnlockedLevel
+    }
+    
+    @discardableResult
+    mutating func advance(to level: Int) -> Bool {
+        if LevelTracker.isUnlocked(level) {
+            currentLevel = level
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Player {
+    var tracker = LevelTracker()
+    let playerName: String
+    func complete(level: Int) {
+        LevelTracker.unlock(level + 1)
+        tracker.advance(to: level + 1)
+    }
+    init(name: String) {
+        playerName = name
+    }
+}
+
+var player = Player(name: "Argyrios")
+player.complete(level: 1)
+print("highest unlocked level is now \(LevelTracker.highestUnlockedLevel)")
+
+player = Player(name: "Beto")
+if player.tracker.advance(to: 6) {
+    print("player is now on level 6")
+} else {
+    print("level 6 has not yet been unlocked")
+}
 
 //: ### 总结
 //:
